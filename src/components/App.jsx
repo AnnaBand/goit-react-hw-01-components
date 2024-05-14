0109859eb80d6bb798ef0a6066616573c216d9be
KrolLew23 <aniabandola@gmail.com>
2024-05-14 15:08:02 +0200

    date

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from './Profile/Profile';
 import user from './Profile/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data.json';
 
 export const App = () => {
   return (
@@ -13,6 +15,7 @@ export const App = () => {
       >
         <p>tekst</p>
       </Profile>
+      <Statistics title={data.title} stats={data}></Statistics>
       
     </div>
   );
