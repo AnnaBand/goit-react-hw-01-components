819af0339b6b4200c7b212dd4820f85b44e20a20
KrolLew23 <aniabandola@gmail.com>
2024-05-14 15:14:45 +0200

    friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Profile } from './Profile/Profile';
 import user from './Profile/user.json';
 import { Statistics } from './Statistics/Statistics';
 import data from './Statistics/data.json';
+import { FriendsList } from './FriendsList/FriendsList';
+import friends from './FriendsList/friends.json';
 
 export const App = () => {
   return (
@@ -16,6 +18,7 @@ export const App = () => {
         <p>tekst</p>
       </Profile>
       <Statistics title={data.title} stats={data}></Statistics>
+      <FriendsList friends={friends}></FriendsList>
       
     </div>
   );
